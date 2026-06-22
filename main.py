01bc89cb27ef3af7b6a74101f911920203a94d7f
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 04:32:45 +0200

    Initialize main.py for Pyrogram bot

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyrogram import Client
+
+from config import *
+
+app = Client(
+    "HMD-ALSOURY-BEST",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+from plugins.start import register as start_register
+
+start_register(app)
+
+print("HMD-ALSOURY-BEST Started")
+
+app.run()
