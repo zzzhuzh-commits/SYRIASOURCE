ce279934a1ffa34ef9d77047194b09e77aab463a
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 04:31:27 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
+
+MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+
+MUST_JOIN_CHANNEL = os.getenv("MUST_JOIN_CHANNEL")
+
+BOT_NAME = "HMD-ALSOURY-BEST"
